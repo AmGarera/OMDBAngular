7eebaaf91d68ce6b43997f38293267dff7b54b85
amgmetallica <gareraanthony@gmail.com>
2016-09-05 12:20:34 -0400

    Run NPM install and Bower install before running

---
+++ app/js/app.js
@@ -0,0 +1,37 @@
+'use strict';
+
+// Declare app level module which depends on views, and components
+angular.module('myApp', [])
+    .controller('MovieController', function ($scope, $http) {
+      $scope.$watch('search', function () {
+        fetch();
+      });
+
+      $scope.search = "Sherlock Holmes";
+
+      function fetch() {
+        $http.get("http://www.omdbapi.com/?t=" + $scope.search + "&tomatoes=true&plot=full")
+            .then(function (response) {
+              $scope.details = response.data;
+            });
+
+        $http.get("http://www.omdbapi.com/?s=" + $scope.search)
+            .then(function (response) {
+              $scope.related = response.data;
+            });
+      }
+
+      $scope.update = function (movie) {
+        $scope.search = movie.Title;
+      };
+
+      $scope.select = function () {
+        this.setSelectionRange(0, this.value.length);
+      }
+            });
+
+config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
+  $locationProvider.hashPrefix('!');
+
+  $routeProvider.otherwise({redirectTo: '/view1'});
+}]);
